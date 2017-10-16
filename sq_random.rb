b4f2dc7d3e21d6f5086193e200122d7f1ccd0ac1
jordanberghofer <jordan.berghofer@gmail.com>
2017-10-16 14:00:36 -0500

    jk, this is the finished product

--- sq_random.rb
+++ sq_random.rb
@@ -13,19 +13,3 @@ for numbers in random_numbers
 end
 
 puts squared_numbers
-
-
-# # Using the Random class, generate a list of 20 random numbers between 0 and 49.
-# prng = Random.new
-
-
-# random_numbers = 20.times.map{prng.rand 50}
-# puts(random_numbers)
-# puts "***********"
-# # With the resulting array, build a new array that contains each number squared. For example, if the original list is [2, 5], the final list will be [4, 25].
-# squared = []
-# for num in random_numbers
-#     square_it = num * num
-#     squared << square_it
-# end
-# puts squared
